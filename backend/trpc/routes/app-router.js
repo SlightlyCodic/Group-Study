9e2a8d63a1e48f840238b2e77c963a830896a2f4
SlightlyCodic <chiragj831@gmail.com>
2025-07-21 00:39:39 +0100

    health checkpoint

--- backend/trpc/routes/app-router.js
+++ backend/trpc/routes/app-router.js
@@ -1,14 +1,14 @@
 import { createTRPCRouter } from "./create-context.js";
-import hiRoute from "./routes/example/hi/route.js";
-import createUserRoute from "./routes/users/create/route.js";
-import updateStatusRoute from "./routes/users/update-status/route.js";
-import createGroupRoute from "./routes/groups/create/route.js";
-import joinGroupRoute from "./routes/groups/join/route.js";
-import leaveGroupRoute from "./routes/groups/leave/route.js";
-import getUserGroupsRoute from "./routes/groups/get-user-groups/route.js";
-import createNotificationsRoute from "./routes/notifications/create/route.js";
-import getUserNotificationsRoute from "./routes/notifications/get-user-notifications/route.js";
-import markNotificationReadRoute from "./routes/notifications/mark-read/route.js";
+import hiRoute from "./example/hi/route.js";
+import createUserRoute from "./users/create/route.js";
+import updateStatusRoute from "./users/update-status/route.js";
+import createGroupRoute from "./groups/create/route.js";
+import joinGroupRoute from "./groups/join/route.js";
+import leaveGroupRoute from "./groups/leave/route.js";
+import getUserGroupsRoute from "./groups/get-user-groups/route.js";
+import createNotificationsRoute from "./notifications/create/route.js";
+import getUserNotificationsRoute from "./notifications/get-user-notifications/route.js";
+import markNotificationReadRoute from "./notifications/mark-read/route.js";
 
 export const appRouter = createTRPCRouter({
   example: createTRPCRouter({
